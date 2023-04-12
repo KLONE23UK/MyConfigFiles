cd185ec78ad2168ec83e024a394b89fddf31a935
Ryan Gilluley <stillactivity@protonmail.com>
2023-04-12 19:43:33 +0100

    Add fix for transparent bg theme in .vimrc

--- Supplamentery/getLatestHeadline.sh
+++ Supplamentery/getLatestHeadline.sh
@@ -3,9 +3,10 @@
 
 BASEURL='https://newsapi.org/v2/'
 COUNTRY='gb'
-SOURCE='?id=ign'
 APIKEY='02d9e67622ea496ebcee31af916583c7'
-GETNEWS=$(curl -s "${BASEURL}top-headlines?country=${COUNTRY}&source${SOURCE}&apiKey=${APIKEY}")
+CATAGORY='general'
+GETNEWS=$(curl -s "${BASEURL}top-headlines?country=${COUNTRY}&catagory=${CATAGORY}&apiKey=${APIKEY}")
+echo -e "${BASEURL}top-headlines?country=${COUNTRY}&catagory=${CATAGORY}&apiKey=${APIKEY}"
 
 echo $GETNEWS | jq -r '.articles [1] | .title'
 
