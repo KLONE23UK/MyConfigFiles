dced60178260bf9c45ac3dfa7eb3bc0da0af1d0c
Ryan Gilluley <stillactivity@protonmail.com>
2023-05-20 00:34:37 +0100

    Update scripts to become full cli scripts

---
+++ Scripts/rg_setup_01.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+### Setup ZSH ###
+### Ryan Gilluley ###
+
+## Options
+
+
+## Bash
+
+# Edit .bashrc
+
+
+## ZSH
+
+# Get ZSH
+
+# Clone and setup Oh-My-Zsh
+
+# Clone ZSH Auto suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+## Config Files
+
+# .zsh
+wget -P ~/ https://e.pcloud.link/publink/show?code=XZ5F0uZ6LcYYGMw8sVADaX3qVy04zxjd6YX
+
+## Check Rebuild
+
